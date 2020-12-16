626567277e317330bac5ac87311e26769c5ece57
mathewgries <mathewgries@gmail.com>
2020-12-15 21:49:30 -0500

    Adding some faulty code

--- services/watchlists/get.js
+++ services/watchlists/get.js
@@ -7,7 +7,7 @@ export const main = handler(async (event, context) => {
     // 'Key' defines the partition key and sort key of the item to be retrieved
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      watchlistId: event.pathParameters.id, // The id of the note from the path
+      // watchlistId: event.pathParameters.id, // The id of the note from the path
     },
   };
 
